df05da8138edc8647e7ef8bcd8d8265b2a3c8bbd
Mateus Ferrari <mateusfaa@hotmail.com>
2024-12-03 19:19:24 -0300

    test: create first test to check if website is online

---
+++ selenium-test/src/test/java/SeleniumTest.java
@@ -0,0 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SeleniumTest {
+  private WebDriver driver;
+  private String BASE_URL = "https://test-a-pet.vercel.app/";
+
+  @BeforeEach
+  void setUp() {
+    WebDriverManager.chromedriver().setup();
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--remote-allow-origins=*");
+    driver = new ChromeDriver(options);
+  }
+
+  @Test
+  void testSiteIsOnline() {
+    final var softly = new SoftAssertions();
+    driver.get(BASE_URL);
+    String currentUrl = driver.getCurrentUrl();
+    System.out.println("URL carregada: " + currentUrl);
+    softly.assertThat(currentUrl).isEqualTo(BASE_URL);;
+    driver.quit();
+
+  }
+}
